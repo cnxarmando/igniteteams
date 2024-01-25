a0d35e4d7218245ca7ce5f6f04c9d60834a328d3
Armando Freitas <62257366+cnxarmando@users.noreply.github.com>
2024-01-24 21:33:15 -0500

    Initial groups structure

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles'
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
