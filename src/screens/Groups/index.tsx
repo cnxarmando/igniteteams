888924e907399704226401aa6a5c7594652b09a6
Armando Freitas <62257366+cnxarmando@users.noreply.github.com>
2024-01-29 21:06:18 -0500

    Updated configuration

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, View, FlatList } from 'react-native';
 import { styles } from './styles'
 import { Header } from '@/components/Header';
 import { HighLight } from '@/components/HighLight';
+import { GroupCard } from '@/components/GroupCard';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <View style={styles.container}>
       <Header />
@@ -11,7 +15,16 @@ export function Groups() {
         title='Turmas'
         subTitle='Jogue com sua turma'
       />
-      
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+          )
+        }
+      />
     </View>
   );
 }
